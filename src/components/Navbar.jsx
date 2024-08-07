940efe9805b9b3207437c7013ca3424e3d4b2fff
Prashidhika Neupane <mununeupane8@gmail.com>
2024-08-07 20:28:28 +0100

    lab done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,39 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <Link to="/">
-      <nav style={{ background: "cornflowerblue", textAlign: "center", padding: "10px 40px" }}>
+    <nav
+      style={{
+        background: "cornflowerblue",
+        textAlign: "center",
+        padding: "10px 40px",
+      }}
+    >
+      <Link to="/">
         <img
           src={homeIcon}
           alt="home icon"
           style={{ height: "30px", width: "auto" }}
         />
-      </nav>
-    </Link>
+      </Link>
+      <Link
+        to="/beers"
+        style={{ margin: "15px", color: "white", textDecoration: "none" }}
+      >
+        All Beers
+      </Link>
+      <Link
+        to="/random-beer"
+        style={{ margin: "15px", color: "white", textDecoration: "none" }}
+      >
+        Random Beer
+      </Link>
+      <Link
+        to="/new-beer"
+        style={{ margin: "15px", color: "white", textDecoration: "none" }}
+      >
+        Add New Beer
+      </Link>
+    </nav>
   );
 }
 
